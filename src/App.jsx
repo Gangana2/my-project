07a3d7971a0d38bb97fcd21ba0cb246fdf2d159a
Gangana Fernando <167875165+Gangana2@users.noreply.github.com>
2025-08-26 23:26:06 +0530

    my first commit
    first ever commit in this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import CartPage from './Pages/CartPage';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div>
+      <Home />
       <CartPage />
     </div>//
   );
